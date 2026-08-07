9a6bad2e597833168e6a43a9da5933e703e0f10f
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-07 04:02:03 +0200

    Dodanie logowania klientów B2B

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+export default function Login(){
+  return (
+    <main>
+      <h1>Logowanie Profesja Premium Limited™</h1>
+
+      <form>
+        <input placeholder="Adres e-mail" />
+        <input placeholder="Hasło" type="password" />
+        <button>Zaloguj</button>
+      </form>
+    </main>
+  );
+}
