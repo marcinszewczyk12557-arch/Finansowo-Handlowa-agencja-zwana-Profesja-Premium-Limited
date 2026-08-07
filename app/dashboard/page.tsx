df4f490a7b78d192b28031967d02528564475e75
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-07 04:02:06 +0200

    Dodanie panelu klienta B2B

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+export default function Dashboard(){
+  return (
+    <main>
+      <h1>Panel Klienta B2B</h1>
+
+      <section>
+        <h2>Twoje zapytania ofertowe</h2>
+        <p>Brak aktywnych zapytań.</p>
+        <button>Nowe zapytanie</button>
+      </section>
+    </main>
+  );
+}
