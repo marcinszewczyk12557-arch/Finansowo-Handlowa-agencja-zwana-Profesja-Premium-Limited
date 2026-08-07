a6c542f2ced767199d70eba84192abf2b7c67514
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-07 04:01:58 +0200

    Dodanie rejestracji klientów B2B

---
+++ app/register/page.tsx
@@ -0,0 +1,15 @@
+export default function Register(){
+  return (
+    <main>
+      <h1>Rejestracja Partnera B2B</h1>
+
+      <form>
+        <input placeholder="Nazwa firmy" />
+        <input placeholder="Imię i nazwisko" />
+        <input placeholder="Adres e-mail" />
+        <input placeholder="Hasło" type="password" />
+        <button>Utwórz konto</button>
+      </form>
+    </main>
+  );
+}
