8adbc6f9eb0bb237c420784d2fca12cf622b3198
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-07 04:02:43 +0200

    Dodanie obsługi zapytań ofertowych administratora

---
+++ app/admin/offers/page.tsx
@@ -0,0 +1,9 @@
+export default function AdminOffers(){
+  return (
+    <main>
+      <h1>Zapytania ofertowe</h1>
+      <p>Obsługa zapytań klientów B2B.</p>
+      <button>Przygotuj ofertę</button>
+    </main>
+  );
+}
