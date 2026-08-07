55238393d27a25cd4b7bd9ed28c3089a52c78149
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-07 03:59:09 +0200

    Dodanie komponentu nagłówka Profesja Premium Limited

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header(){
+  return (
+    <header>
+      <h1>Profesja Premium Limited™</h1>
+      <nav>
+        <a>Start</a>
+        <a>Oferta</a>
+        <a>B2B</a>
+        <a>Kontakt</a>
+        <a>Logowanie</a>
+      </nav>
+    </header>
+  );
+}
