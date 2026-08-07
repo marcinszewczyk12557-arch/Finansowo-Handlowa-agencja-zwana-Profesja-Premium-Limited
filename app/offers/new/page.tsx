371d04d5636747d306f82800ba2550da29fb7867
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-07 04:01:10 +0200

    Dodanie strony nowych zapytań ofertowych B2B

---
+++ app/offers/new/page.tsx
@@ -0,0 +1,10 @@
+import OfferForm from '@/components/OfferForm';
+
+export default function NewOffer(){
+  return (
+    <main>
+      <h1>Profesja Premium Limited™</h1>
+      <OfferForm />
+    </main>
+  );
+}
