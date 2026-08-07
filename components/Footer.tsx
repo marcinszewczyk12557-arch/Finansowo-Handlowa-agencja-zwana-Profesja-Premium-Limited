c4190abe7c6dcd4baeea907237f5c4cd63309a8a
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-07 05:38:54 +0200

    Add Footer component with company information

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,27 @@
+export default function Footer() {
+  return (
+    <footer>
+      <h3>Metropolis Corp. projekt</h3>
+      <p>Profesja Premium Limited™</p>
 
+      <p>
+        ul. Bardowskiego bud. 10 lok. 8<br />
+        58-303 Wałbrzych<br />
+        Dolnośląskie, Polska
+      </p>
+
+      <p>
+        E-mail: marcin.szewczyk12557@gmail.com
+      </p>
+
+      <p>
+        Telefon: +48 886 636 981
+      </p>
+
+      <p>
+        ©™ Profesja Premium Limited™<br />
+        Wszelkie prawa zastrzeżone.
+      </p>
+    </footer>
+  );
+}
