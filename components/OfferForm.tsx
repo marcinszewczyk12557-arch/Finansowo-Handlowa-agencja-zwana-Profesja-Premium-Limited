15c1044e92f4b42c82a35a75d932c4f4e8a80077
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-07 04:01:05 +0200

    Dodanie formularza zapytania ofertowego B2B

---
+++ components/OfferForm.tsx
@@ -0,0 +1,13 @@
+export default function OfferForm(){
+  return (
+    <form>
+      <h2>Zapytanie ofertowe B2B</h2>
+
+      <input placeholder="Nazwa produktu" />
+      <input placeholder="Ilość" />
+      <textarea placeholder="Dodatkowe wymagania" />
+
+      <button>Wyślij zapytanie</button>
+    </form>
+  );
+}
