4c05d8ac8d0fab9fef3199a104cf39d0bbecd635
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-07 03:58:32 +0200

    Dodanie głównego layoutu aplikacji Profesja Premium Limited

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Profesja Premium Limited™',
+  description: 'Finansowo-Handlowa Agencja Profesja Premium Limited™'
+};
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return (
+    <html lang="pl">
+      <body>{children}</body>
+    </html>
+  );
+}
