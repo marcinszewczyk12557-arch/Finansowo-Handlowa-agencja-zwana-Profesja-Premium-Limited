541a57a6ce462f533c6b63e550eeba53c6257c03
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-07 03:59:19 +0200

    Dodanie katalogu produktów Profesja Premium Limited

---
+++ data/products.ts
@@ -0,0 +1,18 @@
+const products = [
+  {
+    id: 1,
+    name: 'Produkt Premium',
+    description: 'Oferta z globalnego katalogu Profesja Premium Limited™',
+    category: 'Premium',
+    price: 'Zapytaj o cenę'
+  },
+  {
+    id: 2,
+    name: 'Oferta B2B',
+    description: 'Indywidualne warunki współpracy dla firm',
+    category: 'B2B',
+    price: 'Oferta indywidualna'
+  }
+];
+
+export default products;
