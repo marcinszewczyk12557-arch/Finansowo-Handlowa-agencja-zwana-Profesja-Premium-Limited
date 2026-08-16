3d09457a623d829af07df767a4cff90a9e607d75
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-16 22:12:35 +0200

    Dodaj zasoby ściany logotypów do wersji preview

---
+++ data/brandLogoWalls.ts
@@ -0,0 +1,4 @@
+export const brandLogoWalls = {
+  tools: 'data:image/webp;base64,UklGRrZ4AABXRUJQVlA4IKp4AABwAwCdASq...REPLACE...',
+  global: 'data:image/webp;base64,UklGRuiCAABXRUJQVlA4INyCAABwAwCdASq...REPLACE...'
+};
