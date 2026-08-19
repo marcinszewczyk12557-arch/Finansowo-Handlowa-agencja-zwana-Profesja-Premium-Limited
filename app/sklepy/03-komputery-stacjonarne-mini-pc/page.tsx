0f5ea03d9a5d3f092b00270c5b2935856af599b4
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-20 01:38:33 +0200

    feat(store-03): add desktops and mini PC route

---
+++ app/sklepy/03-komputery-stacjonarne-mini-pc/page.tsx
@@ -0,0 +1,3 @@
+import StoreCatalogTemplate from '../../../components/StoreCatalogTemplate';
+import {STORES_02_06} from '../../../lib/stores02to06';
+export default function Page(){return <StoreCatalogTemplate store={STORES_02_06[1]}/>}
