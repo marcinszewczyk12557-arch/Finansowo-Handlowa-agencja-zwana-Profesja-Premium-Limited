86da344a60f0c38e4779cb0e3f49e0c430d6aae9
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-20 19:22:17 +0200

    feat(store14): add video surveillance assortment tree

---
+++ lib/store14.ts
@@ -0,0 +1,15 @@
+import type {StoreSpec} from './stores02to06';
+
+export const STORE_14: StoreSpec = {
+  id:'SKLEP-014',
+  slug:'14-monitoring-wizyjny',
+  name:'Monitoring wizyjny',
+  tree:[
+    {group:'Kamery i obrazowanie',types:['kamery kopułkowe IP','kamery tubowe IP','kamery PTZ','kamery wielosensorowe','kamery termowizyjne do zastosowań przemysłowych po kwalifikacji wymagań']},
+    {group:'Rejestracja i przechowywanie',types:['rejestratory NVR','serwery VMS','macierze i pamięć do archiwizacji wideo','stacje operatorskie','systemy kopii zapasowych nagrań']},
+    {group:'Analiza obrazu',types:['detekcja ruchu i zdarzeń','analityka przekroczenia stref','liczenie osób i obiektów','analityka kolejek i obłożenia','integracja zdarzeń z systemami bezpieczeństwa']},
+    {group:'Transmisja i infrastruktura',types:['przełączniki PoE','transmisja światłowodowa','konwertery i extendery','zasilanie awaryjne UPS','obudowy i akcesoria instalacyjne']},
+    {group:'Centra dozoru',types:['monitory operatorskie','ściany wizyjne','konsole i stanowiska operatorskie','klawiatury sterujące PTZ','systemy zarządzania alarmami i zdarzeniami']},
+    {group:'Zastosowania specjalistyczne',types:['monitoring dla magazynów i logistyki','monitoring obiektów biurowych i handlowych','monitoring przemysłowy','monitoring terenów zewnętrznych','monitoring infrastruktury wymagającej kwalifikacji prywatności i bezpieczeństwa']}
+  ]
+};
