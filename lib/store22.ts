798b1ef442a9aeb9e846e95df0d790ce96a124b6
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-21 18:47:00 +0200

    feat: add store 22 industrial cleaning systems

---
+++ lib/store22.ts
@@ -0,0 +1,15 @@
+import type {StoreSpec} from './stores02to06';
+
+export const STORE_22: StoreSpec = {
+  id:'SKLEP-022',
+  slug:'22-systemy-czyszczenia-przemyslowego',
+  name:'Systemy czyszczenia przemysłowego',
+  tree:[
+    {group:'Mycie ciśnieniowe i wysokociśnieniowe',types:['profesjonalne myjki zimno- i gorącowodne','agregaty wysokociśnieniowe','systemy stacjonarne i mobilne','akcesoria do zastosowań przemysłowych','dobór po analizie medium, zabrudzeń, powierzchni i wymaganego ciśnienia']},
+    {group:'Czyszczenie posadzek i dużych powierzchni',types:['automaty szorująco-zbierające','zamiatarki przemysłowe','maszyny jednotarczowe','systemy autonomiczne i półautonomiczne','wydajność oraz kompatybilność wymagają potwierdzenia dla konkretnego obiektu']},
+    {group:'Odkurzanie i odpylanie przemysłowe',types:['odkurzacze przemysłowe','systemy centralnego odkurzania','separatory i odpylacze','rozwiązania do pyłów i odpadów procesowych','klasa filtracji i zastosowanie wymagają weryfikacji dokumentacji']},
+    {group:'Czyszczenie specjalistyczne',types:['mycie parowe','czyszczenie suchym lodem','systemy ultradźwiękowe','mycie części i komponentów','technologia dobierana do materiału, procesu i wymagań bezpieczeństwa']},
+    {group:'Automatyka, dozowanie i gospodarka mediami',types:['systemy dozowania chemii','kontrola stężenia i przepływu','odzysk i filtracja wody procesowej','monitoring eksploatacyjny','integracja z infrastrukturą obiektu po potwierdzeniu kompatybilności']},
+    {group:'Zastosowania B2B',types:['zakłady produkcyjne','magazyny i centra logistyczne','przemysł spożywczy i procesowy','warsztaty i zaplecza techniczne','projekty wymagające analizy procesu, BHP, zgodności, dokumentacji, ceny i dostępności przed wiążącą ofertą']}
+  ]
+};
