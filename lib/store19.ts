ef9bd8994ffc2282452caf569dc389fb236837a4
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-21 13:37:18 +0200

    feat: add store 19 HVAC and air conditioning catalog

---
+++ lib/store19.ts
@@ -0,0 +1,15 @@
+import type {StoreSpec} from './stores02to06';
+
+export const STORE_19: StoreSpec = {
+  id:'SKLEP-019',
+  slug:'19-hvac-klimatyzacja',
+  name:'HVAC i klimatyzacja',
+  tree:[
+    {group:'Klimatyzacja komercyjna',types:['systemy split i multi-split dla przedsiębiorstw','systemy VRF/VRV','jednostki kasetonowe i kanałowe','klimatyzacja precyzyjna','dobór po analizie obciążeń cieplnych, projektu i dokumentacji konkretnego modelu']},
+    {group:'Wentylacja i świeże powietrze',types:['centrale wentylacyjne','rekuperacja komercyjna','wentylatory i układy wyciągowe','filtracja powietrza','rozwiązania dobierane po weryfikacji wydajności, sprężu, jakości powietrza i projektu']},
+    {group:'Chłodzenie i woda lodowa',types:['agregaty wody lodowej','fan-coile','dry coolery','układy free cooling','konfiguracje wymagające obliczeń hydraulicznych, cieplnych i potwierdzenia parametrów producenta']},
+    {group:'Sterowanie i automatyka HVAC',types:['sterowniki HVAC','czujniki temperatury, wilgotności i CO2','integracja BMS','harmonogramowanie i optymalizacja energii','integracje BACnet/Modbus po potwierdzeniu kompatybilności']},
+    {group:'Jakość i uzdatnianie powietrza',types:['filtry i sekcje filtracyjne','oczyszczanie powietrza','kontrola wilgotności','monitoring jakości powietrza','rozwiązania wymagające weryfikacji zastosowania, dokumentacji i wymagań obiektu']},
+    {group:'Zastosowania B2B',types:['biurowce i obiekty usługowe','hale produkcyjne i magazyny','hotele i handel','serwerownie i pomieszczenia techniczne','projekty wymagające audytu obiektu, bilansu energetycznego, zgodności i dokumentacji przed wiążącą ofertą']}
+  ]
+};
