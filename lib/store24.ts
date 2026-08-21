6f8a0943d49dfd9d43cca2cfb0451bbd101b24cd
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-22 01:04:37 +0200

    feat: add store 24 metrology and measurement systems

---
+++ lib/store24.ts
@@ -0,0 +1,15 @@
+import type {StoreSpec} from './stores02to06';
+
+export const STORE_24: StoreSpec = {
+  id:'SKLEP-024',
+  slug:'24-systemy-pomiarowe-metrologia',
+  name:'Systemy pomiarowe i metrologia',
+  tree:[
+    {group:'Pomiary wymiarowe i geometryczne',types:['suwmiarki i mikrometry profesjonalne','wysokościomierze i czujniki','maszyny współrzędnościowe CMM','systemy optyczne i wizyjne','zakres, dokładność i niepewność pomiaru wymagają potwierdzenia dla konkretnego procesu']},
+    {group:'Pomiary elektryczne i elektroniczne',types:['multimetry i mierniki laboratoryjne','oscyloskopy i analizatory','mierniki izolacji i bezpieczeństwa','analizatory jakości energii','kategoria pomiarowa, pasmo i parametry bezpieczeństwa wymagają weryfikacji']},
+    {group:'Temperatura, ciśnienie i przepływ',types:['termometry i kamery termowizyjne','kalibratory temperatury','manometry i przetworniki','przepływomierze i kalibratory procesu','dobór zależy od medium, zakresu, środowiska i wymaganej niepewności']},
+    {group:'Metrologia masy i siły',types:['wagi przemysłowe i laboratoryjne','wzorce masy','siłomierze i stanowiska testowe','systemy ważenia procesowego','klasa dokładności i procedura wzorcowania wymagają potwierdzenia']},
+    {group:'Kalibracja i zarządzanie jakością',types:['kalibratory wielofunkcyjne','wzorce odniesienia','oprogramowanie ewidencji wyposażenia','stanowiska i procedury kalibracyjne','spójność pomiarowa i dokumentacja muszą być ocenione dla konkretnego zastosowania']},
+    {group:'Zastosowania B2B',types:['produkcja i kontrola jakości','laboratoria i działy R&D','energetyka i utrzymanie ruchu','automotive, lotnictwo i infrastruktura','projekty wymagające weryfikacji parametrów, dokumentacji, warunków środowiskowych, ceny i dostępności przed wiążącą ofertą']}
+  ]
+};
