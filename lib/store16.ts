3d7e257740c0a25d49dadb650706ca6db4a803df
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-21 10:38:00 +0200

    feat: add store 16 photovoltaic systems catalog

---
+++ lib/store16.ts
@@ -0,0 +1,15 @@
+import type {StoreSpec} from './stores02to06';
+
+export const STORE_16: StoreSpec = {
+  id:'SKLEP-016',
+  slug:'16-fotowoltaika',
+  name:'Fotowoltaika',
+  tree:[
+    {group:'Moduły fotowoltaiczne',types:['moduły monokrystaliczne','moduły bifacjalne','moduły do instalacji komercyjnych i przemysłowych','moduły do zastosowań specjalnych po kwalifikacji projektu','komponenty wymagające weryfikacji dokumentacji konkretnego modelu']},
+    {group:'Falowniki i konwersja energii',types:['falowniki stringowe','falowniki centralne','mikrofalowniki','optymalizatory mocy','urządzenia dobierane po weryfikacji parametrów sieci i projektu']},
+    {group:'Konstrukcje i montaż',types:['systemy dachowe','systemy gruntowe','carporty fotowoltaiczne','konstrukcje dla obiektów przemysłowych','elementy montażowe dobierane po analizie obciążeń i podłoża']},
+    {group:'Rozdział, ochrona i okablowanie',types:['rozdzielnice DC i AC','zabezpieczenia przepięciowe','aparatura łączeniowa','okablowanie solarne i złącza','elementy ochronne dobierane zgodnie z projektem instalacji']},
+    {group:'Monitoring i zarządzanie',types:['monitoring produkcji','bramki komunikacyjne','systemy zarządzania energią','integracja z BMS i EMS po weryfikacji protokołów','analityka i diagnostyka instalacji']},
+    {group:'Zastosowania B2B',types:['instalacje dla przedsiębiorstw','hale i magazyny','obiekty handlowe i biurowe','farmy fotowoltaiczne','projekty wymagające audytu, projektu, warunków przyłączenia i kwalifikacji zgodności przed wiążącą ofertą']}
+  ]
+};
