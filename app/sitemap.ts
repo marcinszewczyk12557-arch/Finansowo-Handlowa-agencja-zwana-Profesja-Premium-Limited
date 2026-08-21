302c3c1b1f5aaa3fc7063d7deb79a207877b2c69
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-21 20:39:30 +0200

    feat: add store 23 to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -16,7 +16,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/sklepy/16-fotowoltaika', '/sklepy/17-magazyny-energii',
     '/sklepy/18-ladowanie-pojazdow-elektrycznych', '/sklepy/19-hvac-klimatyzacja',
     '/sklepy/20-pompy-ciepla', '/sklepy/21-uzdatnianie-filtracja-wody',
-    '/sklepy/22-systemy-czyszczenia-przemyslowego',
+    '/sklepy/22-systemy-czyszczenia-przemyslowego', '/sklepy/23-narzedzia-przemyslowe',
     '/enterprise-equipment', '/biov-era', '/heavy-equipment', '/offers/new',
     '/about', '/contact', '/terms', '/privacy',
   ];
@@ -32,7 +32,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/sklepy/16-fotowoltaika', '/sklepy/17-magazyny-energii',
     '/sklepy/18-ladowanie-pojazdow-elektrycznych', '/sklepy/19-hvac-klimatyzacja',
     '/sklepy/20-pompy-ciepla', '/sklepy/21-uzdatnianie-filtracja-wody',
-    '/sklepy/22-systemy-czyszczenia-przemyslowego',
+    '/sklepy/22-systemy-czyszczenia-przemyslowego', '/sklepy/23-narzedzia-przemyslowe',
     '/enterprise-equipment', '/biov-era', '/heavy-equipment'
   ]);
 
