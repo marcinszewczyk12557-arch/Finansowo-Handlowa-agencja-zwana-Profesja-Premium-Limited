20c4bc8fc1ef5ac81525d6164e3cdec9591dacd4
dropshipping Profesja Premium Limited <marcin.szewczyk12557@gmail.com>
2026-08-22 12:45:35 +0200

    feat: add store 026 professional tools RFQ tree

---
+++ lib/store26.ts
@@ -0,0 +1,15 @@
+import type {StoreSpec} from './stores02to06';
+
+export const STORE_26: StoreSpec = {
+  id:'SKLEP-026',
+  slug:'26-narzedzia-profesjonalne',
+  name:'Narzędzia profesjonalne',
+  tree:[
+    {group:'Elektronarzędzia profesjonalne',types:['wiertarko-wkrętarki i młotowiertarki','klucze udarowe','szlifierki i polerki','pilarki i przecinarki','moc, zasilanie, osprzęt i warunki pracy wymagają potwierdzenia dla konkretnego zastosowania']},
+    {group:'Narzędzia ręczne i montażowe',types:['zestawy narzędzi serwisowych','klucze i nasadki','narzędzia dynamometryczne','narzędzia izolowane i specjalistyczne','zakres pracy, dokładność i wymagane normy muszą zostać zweryfikowane']},
+    {group:'Obróbka i przygotowanie materiału',types:['wiercenie i gwintowanie','cięcie i szlifowanie','frezowanie i obróbka wykończeniowa','materiały ścierne i eksploatacyjne','materiał, tolerancje, wydajność i kompatybilność osprzętu wymagają weryfikacji']},
+    {group:'Systemy akumulatorowe i zasilanie',types:['platformy akumulatorowe','ładowarki wielostanowiskowe','akumulatory przemysłowe','zasilacze i przetwornice warsztatowe','kompatybilność, cykl pracy i parametry elektryczne muszą być potwierdzone']},
+    {group:'Gospodarka narzędziowa i BHP',types:['wózki i szafy narzędziowe','systemy wydawania i ewidencji narzędzi','organizery i wyposażenie stanowisk','akcesoria ochronne związane z pracą narzędziową','dobór wymaga oceny stanowiska, ryzyka i procedur zakładowych']},
+    {group:'Zastosowania B2B',types:['utrzymanie ruchu i serwis','produkcja i montaż','budownictwo i instalacje','warsztaty i floty techniczne','konkretna marka, model, parametry, dokumentacja, cena i dostępność są publikowane dopiero po weryfikacji źródłowej']}
+  ]
+};
